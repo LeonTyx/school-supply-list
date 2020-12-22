53111be985b50931b011fbd50e0ac209306ec800
leont <leontyx@gmail.com>
2020-12-21 23:36:36 -0800

    Moved from global db to dep. injection
    -Removed database.go which previously held the global DBCon variabled
    -Introduced dependency injection

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
-	"./database"
+	"database/sql"
+	"fmt"
+	_ "github.com/antonlindstrom/pgstore"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"log"
+	"os"
 )
+
+type DB struct {
+	db *sql.DB
+}
 
 func initEnv()  {
 	err := godotenv.Load("projectvars.env")
@@ -15,5 +23,22 @@ func initEnv()  {
 
 func main() {
 	initEnv()
-	database.InitDB()
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	// if there is an error opening the connection, handle it
+	if err != nil {
+		fmt.Println("Cannot open SQL connection")
+		panic(err.Error())
+	}
+
+	r := gin.Default()
+
+	DBConnection := DB{db: db}
+	r.GET("/ping", DBConnection.dbt)
+	_ = r.Run()
+}
+
+func (env *DB) dbt(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
 }
