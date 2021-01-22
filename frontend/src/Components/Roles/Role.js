249f7581394490064a57db6739441cf3e1f4292b
leont <leontyx@gmail.com>
2021-01-21 19:14:32 -0800

    Added toggleable checkbox

--- frontend/src/Components/Roles/Role.js
+++ frontend/src/Components/Roles/Role.js
@@ -1,19 +1,33 @@
 import React, {useState} from 'react';
 
 function Role(props) {
-    const [role, setRoles] = useState(Object.assign({}, props.role))
+    const [role, setRole] = useState(Object.assign({}, props.role))
+
+    function updatePolicy(resource, policyID){
+        let roleCopy = Object.assign({}, props.role)
+        role.resources[resource].policy[policyID] = !role.resources[resource].policy[policyID]
+        setRole(roleCopy)
+    }
 
     return (
         <div>
             <h3>{role.name}</h3>
             {role.resources != null &&
                 Object.keys(role.resources).map((resourceKey) =>
-                <div>Resource: {resourceKey}
+                <div key={resourceKey}> Resource: {resourceKey}
                     <div>
-                        <input type="checkbox" checked={role.resources[resourceKey].policy.can_add}/>
-                        <input type="checkbox" checked={role.resources[resourceKey].policy.can_view}/>
-                        <input type="checkbox" checked={role.resources[resourceKey].policy.can_edit}/>
-                        <input type="checkbox" checked={role.resources[resourceKey].policy.can_delete}/>
+                        <input type="checkbox"
+                               checked={role.resources[resourceKey].policy.can_add}
+                               onChange={()=>updatePolicy(resourceKey, "can_add")}/>
+                        <input type="checkbox"
+                               checked={role.resources[resourceKey].policy.can_view}
+                               onChange={()=>updatePolicy(resourceKey, "can_view")}/>
+                        <input type="checkbox"
+                               checked={role.resources[resourceKey].policy.can_edit}
+                               onChange={()=>updatePolicy(resourceKey, "can_edit")}/>
+                        <input type="checkbox"
+                               checked={role.resources[resourceKey].policy.can_delete}
+                               onChange={()=>updatePolicy(resourceKey, "can_delete")}/>
                     </div>
                 </div>
             )}
