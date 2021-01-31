0d15bd89e20e8b9621d782ad82977336701dd033
leont <leontyx@gmail.com>
2021-01-31 01:42:05 -0800

    Update supplies.go

--- api/supplies/supplies.go
+++ api/supplies/supplies.go
@@ -11,6 +11,7 @@ import (
 
 type SupplyItem struct {
 	ID       int            `json:"id"`
+	ListID   int            `json:"list_id"`
 	Supply   string         `json:"supply"`
 	Desc     string         `json:"desc"`
 	Category sql.NullString `json:"item_category"`
@@ -24,8 +25,8 @@ func CreateSupply(db *database.DB) gin.HandlerFunc {
 			c.AbortWithStatusJSON(400, "Invalid request.")
 			return
 		}
-		row := db.Db.QueryRow(`INSERT INTO supply_item (supply_name, supply_desc) 
-		  VALUES ($1, $2) RETURNING id`, supply.Supply, supply.Desc)
+		row := db.Db.QueryRow(`INSERT INTO supply_item (list_id, supply_name, supply_desc) 
+		  VALUES ($1, $2, $3) RETURNING id`, supply.ListID, supply.Supply, supply.Desc)
 		err = row.Scan(&supply.ID)
 		if err != nil {
 			database.CheckDBErr(err.(*pq.Error), c)
@@ -45,17 +46,18 @@ func GetSupply(db *database.DB) gin.HandlerFunc {
 			return
 		}
 		supply := SupplyItem{
-			ID: id,
+			ID: -1,
 		}
-
-		row := db.Db.QueryRow(`SELECT supply_name, supply_desc FROM supply_item 
+		row := db.Db.QueryRow(`SELECT id, list_id, supply_name, supply_desc FROM supply_item 
 											WHERE supply_item.id=$1`, id)
-		err = row.Scan(&supply.Supply, &supply.Desc)
+		err = row.Scan(&supply.ID, &supply.ListID, &supply.Supply, &supply.Desc)
 		if err != nil {
 			database.CheckDBErr(err.(*pq.Error), c)
 			return
 		}
-
+		if supply.ID == -1 {
+			c.AbortWithStatusJSON(404, "Supply does not exist")
+		}
 		c.JSON(200, supply)
 	}
 }
@@ -63,14 +65,14 @@ func GetSupply(db *database.DB) gin.HandlerFunc {
 func GetAllSupplies(db *database.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var supplies []SupplyItem
-		rows, err := db.Db.Query(`SELECT id, supply_name, supply_desc FROM supply_item`)
+		rows, err := db.Db.Query(`SELECT id, list_id, supply_name, supply_desc FROM supply_item`)
 		if err != nil {
 			database.CheckDBErr(err.(*pq.Error), c)
 			return
 		}
 		for rows.Next() {
 			var supply SupplyItem
-			err = rows.Scan(&supply.Supply, &supply.Desc, &supply.Desc)
+			err = rows.Scan(&supply.ID, &supply.ListID, &supply.Supply, &supply.Desc)
 			supplies = append(supplies, supply)
 		}
 		c.JSON(200, supplies)
